44a94271baf006a3229b029fd6ae5d39e0757e5f
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-03-31 00:25:20 -0400

    word pattern

--- python/Word Pattern.py
+++ python/Word Pattern.py
@@ -1,2 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        dict = {}
+        s = s.upper()
+        s = s.split(' ')
+        if len(pattern) != len(s):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in dict:
+                dict[pattern[i]] = s[i]
+                if s[i] not in dict:
+                    dict[s[i]] = pattern[i]
+                elif dict[s[i]] != pattern[i]:
+                    return False
+            else:
+                if dict[pattern[i]] != s[i]:
+                    return False
+            
+        return True
